64b54f0aef146c4ab999e347b54eba5d95e18e58
mcrae <mpetrey@gmail.com>
2019-07-10 13:41:22 -0400

    More practice with HAML.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,34 @@
 class ArticlesController <ApplicationController
 
+
+    def index
+    end
+
     def new
         @article = Article.new
     end
 
-    def index
+    def create
+        @article = Article.new(article_params)
+        if @article.save
+         flash[:notice] = "Article was successfully created"
+         redirect_to article_path(@article)
+        else
+         render 'new'
+        end
+       end
+
+    def show 
+        @article = Article.find(params[:id])
     end
+
+    # private methods are for internal usage within the defining class    
+
+    private
+
+        def article_params  
+            params.require(:article).permit(:title, :description)
+        end
+    
 
 end
